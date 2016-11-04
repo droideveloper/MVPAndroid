30d10f44fc194078fedcea22d07a8ef0f3d651b7
droideveloper <fatih.sen.boun@gmail.com>
2016-11-04 23:53:46 +0200

    fixed presenter imp.

--- src/main/java/org/fs/common/AbstractPresenter.java
+++ src/main/java/org/fs/common/AbstractPresenter.java
@@ -15,37 +15,147 @@
  */
 package org.fs.common;
 
-import android.support.annotation.NonNull;
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
-
+import android.view.MenuItem;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import org.fs.util.PreconditionUtility;
 
 public abstract class AbstractPresenter<V extends IView> {
 
   protected final V view;
 
-  public AbstractPresenter(@NonNull final V view) {
+  /**
+   * View interface for accessing its public properties
+   *
+   * @param view view interface extension of IView.
+   */
+  public AbstractPresenter(V view) {
+    PreconditionUtility.checkNotNull(view, "view is null");
     this.view = view;
   }
 
-  protected abstract String   getClassTag();
-  protected abstract boolean  isLogEnabled();
+  /**
+   * View's resume lifecycle
+   */
+  public void onResume()  {}
 
-  protected void log(String str) {
-    log(Log.DEBUG, str);
+  /**
+   * View's pause lifecycle
+   */
+  public void onPause()   {}
+
+  /**
+   * View's start lifecycle
+   */
+  public void onStart()   {}
+
+  /**
+   * View's stop lifecycle
+   */
+  public void onStop()    {}
+
+  /**
+   * View's create lifecycle
+   */
+  public void onCreate()  {}
+
+  /**
+   * View's destroy lifecycle
+   */
+  public void onDestroy() {}
+
+  /**
+   * View's backPressed lifecycle
+   */
+  public void onBackPressed() { }
+
+  /**
+   * View's restoreState lifecycle
+   *
+   * @param restoreState previous state of view.
+   */
+  public void restoreState(Bundle restoreState) {}
+
+  /**
+   * View's storeState lifecycle
+   *
+   * @param storeState current state of view.
+   */
+  public void storeState(Bundle storeState)     {}
+
+  /**
+   * View's activityResult lifecycle
+   *
+   * @param requestCode requestCode of call for result
+   * @param resultCode responseCode of called for result
+   * @param data data of called for result
+   */
+  public void activityResult(int requestCode, int resultCode, Intent data) {}
+
+  /**
+   * View's requestPermissionResult lifecycle for API M and above
+   *
+   * @param requestCode requestCode of call for result
+   * @param permissions permissions of called for result
+   * @param grants grants of called for result
+   */
+  public void requestPermissionResult(int requestCode, String[] permissions, int[] grants) {}
+
+  /**
+   * View's optionsItemSelected lifecycle callbacks
+   *
+   * @param item item that its actions destroyed
+   * @return true if handled false otherwise
+   */
+  public boolean onOptionsItemSelected(MenuItem item) { return false; }
+
+  /**
+   * Log string message with Debug level.
+   *
+   * @param msg a string for log.
+   */
+  protected void log(String msg) {
+    log(Log.DEBUG, msg);
   }
 
+  /**
+   * Log error or exception with Error level using its stackTrace as message.
+   *
+   * @param error an exception for log.
+   */
   protected void log(Throwable error) {
-    StringWriter stringWriter = new StringWriter();
-    PrintWriter  printWriter  = new PrintWriter(stringWriter);
-    error.printStackTrace(printWriter);
-    log(Log.ERROR, stringWriter.toString());
+    StringWriter strWriter = new StringWriter();
+    PrintWriter ptrWriter = new PrintWriter(strWriter);
+    error.printStackTrace(ptrWriter);
+    log(Log.ERROR, strWriter.toString());
   }
 
-  protected void log(int lv, String str) {
-    if(isLogEnabled()) {
-      Log.println(lv, getClassTag(), str);
+  /**
+   * End level of printing logs on android monitor.
+   *
+   * @param level a level of log.
+   * @param msg a string message for log.
+   */
+  protected void log(int level, String msg) {
+    if (isLogEnabled()) {
+      Log.println(level, getClassTag(), msg);
     }
   }
+
+  /**
+   * Logging enabled for this class or not.
+   *
+   * @return a boolean.
+   */
+  protected abstract boolean isLogEnabled();
+
+  /**
+   * Tag for this class in order to use it in logging
+   *
+   * @return a string.
+   */
+  protected abstract String getClassTag();
 }
